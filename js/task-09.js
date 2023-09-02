4f71c07354f6ae5b57a022fe78aa51676d9a8a38
Dawid <dawidpiecha88@gmail.com>
2023-09-02 15:19:57 +0200

    task 09 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,15 @@
+const body = document.body;
+const colorValue = document.querySelector(".widget .color");
+const ColorChangingButton = document.querySelector(".change-color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, "0")}`;
 }
+
+ColorChangingButton.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  colorValue.textContent = randomColor;
+  body.style.backgroundColor = randomColor;
+});

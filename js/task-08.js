330bbdf6362040ef159c3267913e87706af30f25
Dawid <dawidpiecha88@gmail.com>
2023-09-02 16:27:50 +0200

    corected task 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,27 +1,6 @@
-// const registerForm = document.querySelector(".login-form");
+const loginForm = document.querySelector(".login-form");
 
-// registerForm.addEventListener("submit", handleSubmit);
-
-// function handleSubmit(event) {
-//   event.preventDefault();
-//   const form = event.currentTarget;
-//   const email = form.elements.email.value;
-//   const psw = form.elements.psw.value;
-
-//   if (email === "" || password === "") {
-//     return alert("Please fill in all the fields!");
-//   }
-
-//   const User = {
-//     email: email
-//     password: password,
-//   };
-
-//   console.log(User);
-//   frmLogin.reset();}
-const form = document.querySelector(".login-form");
-
-form.addEventListener("submit", (event) => {
+loginForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const email = event.currentTarget.elements.email;
   const password = event.currentTarget.elements.password;
@@ -30,11 +9,11 @@ form.addEventListener("submit", (event) => {
     return alert("All fields should be filled in");
   }
 
-  const objectData = {
+  const object = {
     email: email.value,
     password: password.value,
   };
 
-  console.log(objectData);
+  console.log(object);
   event.currentTarget.reset();
 });

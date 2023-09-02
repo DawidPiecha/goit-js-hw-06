d13301e5156a9c46997b7aa58de4d5f250851f6b
Dawid <dawidpiecha88@gmail.com>
2023-09-02 10:55:12 +0200

    task 01 and 02 done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const ingredientsList = document.querySelector("#ingredients");
+const array = [];
+
+for (const igredient of ingredients) {
+  const listItem = document.createElement("li");
+  listItem.innerHTML = igredient;
+  listItem.classList.add("item");
+  array.push(listItem);
+}
+ingredientsList.append(...array);
